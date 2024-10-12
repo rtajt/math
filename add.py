8f022d28559c8676b55572a958195d930cbd7b7b
rtajt <jamestownselrta@gmail.com>
2024-10-12 00:16:34 -0400

    Create add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+import math
+
+num1 = float(input("What's your first number?"))
+num2 = float(input("What's your second number?"))
+
+def add(x, y):
+    result = num1 + num2
+    print(result)
+add(num1, num2)
