8d223487bab8fb87f2cc6c078d1b60ac6227b880
rtajt <jamestownselrta@gmail.com>
2024-10-12 00:00:37 -0400

    Create divide.py

---
+++ divide.py
@@ -0,0 +1,10 @@
+import math
+
+dividend = float(input("What's your dividend?"))
+divisor = float(input("What's your divisor?"))
+
+def division(x, y):
+    result = x / y
+    print(result)
+
+division(dividend, divisor)
