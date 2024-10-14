4a135a43d6e9f89bfd430ea330ef0c456eeeafef
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2024-10-14 17:51:26 +0000

    let's use returns instead

--- add.py
+++ add.py
@@ -4,6 +4,6 @@ num1 = float(input("What's your first number?"))
 num2 = float(input("What's your second number?"))
 
 def add(x, y):
-    result = num1 + num2
-    print(result)
-add(num1, num2)
+    return num1 + num2
+
+print(add(num1, num2))
--- divide.py
+++ divide.py
@@ -4,7 +4,6 @@ dividend = float(input("What's your dividend?"))
 divisor = float(input("What's your divisor?"))
 
 def division(x, y):
-    result = x / y
-    print(result)
+    return x / y
 
-division(dividend, divisor)
+print(division(dividend, divisor))
