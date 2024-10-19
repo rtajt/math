beb248649acc90b7516b382cf1c5e0eae0e92fe0
rtajt <jamestownselrta@gmail.com>
2024-10-19 01:50:25 -0400

    Add subtract.py

---
+++ subtract.py
@@ -0,0 +1,9 @@
+import math
+
+num1 = float(input("What's your first number? "))
+num2 = float(input("What's your second number? "))
+
+def subtract(x, y):
+    return num1 - num2
+
+print(subtract(num1, num2))
